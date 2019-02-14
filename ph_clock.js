7d49cd4e2c45b6b6c95703204821e13717552c59
Diego Sandoval <dsandoval004@west-mec.org>
2019-02-14 13:53:03 -0700

    Finished
    Created javaScript to countdown the time left, apply it to the HTMl document, then stop the countdown.

--- ph_clock.js
+++ ph_clock.js
@@ -1,3 +1,4 @@
+"use strict ";
 /*
    New Perspectives on HTML5 and CSS3, 7th Edition
    Tutorial 9
@@ -10,11 +11,32 @@
    Filename:   ph_clock.js     
 
 */
-
-
-
-
-
+// Declares all the variable that where used, the minutes left, the seconds left, the time left and the variable that cause the countdown function to be run every second.
+var minsLeft = 30;
+var secsLeft = 0;
+var timeLeft = (minsLeft * 60) + secsLeft;
+var clockID = setInterval('countdown()', 1000);
+// Function that stops the clock when the clock hits zero, and inserts HTML to tell the user that the time has expired.
+function stopClock() {
+    document.getElementById('TimeHead').insertAdjacentHTML('beforeend', "<br />(Order Expired)");
+    clearInterval(clockID);
+};
+// Function to calculate the time left.
+function countdown() {
+    // Calculates the time left and makes sure the it is rounded down.
+    minsLeft = Math.floor(timeLeft / 60);
+    secsLeft = timeLeft - (60 * minsLeft);
+    // Adds a leading zero to the minutes left and the seconds left
+    var minsString = addLeadingZero(minsLeft);
+    var secsString = addLeadingZero(secsLeft);
+    // Applies the minutes left string and the seconds left string to the HTML document.
+    document.getElementById('minutes').innerHTML = minsString;
+    document.getElementById('seconds').innerHTML = secsString;
+    // Runs the function checkTimer() that checks if the timer has reached 0, and if it does hit 0 then it runs the function stopTimer() to stop the timer.
+    checkTimer();
+    // Rudeces the timeLeft by 1 second
+    timeLeft -= 1;
+};
 
 /* ------------------------------------------------- */
 
